3ffc3697559a164fcc44b08244237aebbb3eda4e
spiralizing <spiralizing@gmail.com>
2017-06-22 15:55:18 -0500

    just adding R squared

--- src/func_series.jl
+++ src/func_series.jl
@@ -517,3 +517,12 @@ function rand_series(s::Array{Float64,2}) #este programa hace un random shuffle 
     end
     return out
 end
+####################################################################################################
+function R_squared(s::Array{Float64,2},y::Function)
+  n = size(s)[1]
+  p = mean(s[:,2])
+  σ = sum([(s[i,2]-p)^2 for i=1:n])
+  e = sum([(s[i,2]-y(s[i,1]))^2 for i=1:n])
+  R² = 1 - σ / e
+  return R²
+end
