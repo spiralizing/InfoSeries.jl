9bb255bf42a64427ed29b4325f1e5d1a46a1c7d8
spiralizing <spiralizing@gmail.com>
2018-05-15 09:14:29 -0500

    adding function for swn inizialization

--- src/func_stochastic.jl
+++ src/func_stochastic.jl
@@ -85,3 +85,103 @@ function get_slinks(g, m)
     end
     return indx
 end
+################################################################################
+#Next function is for constructing a small world-like network from a set of groups of blocks.
+function init_swmat(i_groups, ϵ)
+    ng = size(i_groups)[1]
+    ind_g = collect(1:ng)
+    am = zeros(ng,ng)
+    lv = Array{Int64,1}(); push!(lv, 1) #to keep track of the nodes visited
+    il = 1
+    nl = length(lv)
+    counter = 0
+    counter2 = 0
+    tam_res = Array{Int64,1}()
+    stop=false
+    res_nodes = Array{Int64,1}()
+    while stop ==false
+        lg = get_glinks(i_groups[1:end .!= il],i_groups[il][1][end]) #this locates the possible transitions
+        if isempty(lg)
+            println("There are no possible transitions")
+            #break
+            println("Groups ",il, " from ", ng)
+            counter2 += 1
+            if counter2 > 3; stop=true; end
+            continue
+        end
+        l = rand(lg) #chooses one randomly
+        c_v = findin(lv,l) #is checking if the node was already visited
+        #println("Transition from ",il," to ",l)
+        if isempty(c_v) #in the case that the node has not been visited.
+            am[il,l] = 1 #creates a link between the nodes il -> l
+            il = l #now we will move from the new node.
+            push!(lv, il) #adding the node to the visited nodes.
+        else #now for the case that the node has already been visited
+            while length(lg) > 0
+                #println("possible transitions", '\t', lg, "\t node visited before \t",l)
+                filter!(x->x!=l,lg) #removes the node that has been visited
+                if isempty(lg)
+                    counter += 1
+                    res = setdiff(ind_g,lv)
+                    push!(tam_res, length(res))
+                    if counter > ng
+                        #println(counter)
+                        if length(unique(tam_res[end-length(res):end])) == 1
+                            stop=true
+                            res_nodes = res
+                        end
+                    end
+                    #println(res)
+                    il = rand(res)
+                    break
+                end
+                l = rand(lg) #a new node is chosen
+                c_v = findin(l,lv)
+                if isempty(c_v)
+                    am[il,l] = 1
+                    il = l
+                    push!(lv, il)
+                    break
+                #else
+                #    println(lv[c_v],'\t',l,'\t',lg)
+                end
+            end
+            #println("nodes visited:",'\t',nl)
+            #break
+        end
+        nl = length(lv)
+        #println(nl,'\t',"nodes visited")
+    end
+    for i = 1:length(res_nodes)
+        lg = get_glinks(i_groups[1:end .!= res_nodes[i]],i_groups[res_nodes[i]][1][end])
+        if isempty(lg); continue;end
+        l = rand(lg)
+        am[res_nodes[i],l] = 1
+    end
+    for i = 1:ng
+        if isempty(find(x-> x==1, am[i,:]))
+            #println("El nodo ",i," no tiene salida")
+            salidas = get_glinks(i_groups[1:end .!=i],i_groups[i][1][end])
+            if isempty(salidas); continue; end
+            el = rand(get_glinks(i_groups[1:end .!=i],i_groups[i][1][end]))
+            am[i,el] = 1
+        end
+        if isempty(find(x-> x==1, am[:,i]))
+            #println("El nodo ",i," no tiene entrada")
+            entradas = get_slinks(i_groups[1:end .!=i],i_groups[i][1][1])
+            if isempty(entradas); continue;end
+            el = rand(get_slinks(i_groups[1:end .!=i],i_groups[i][1][1]))
+            am[el,i] = 1
+        end
+    end
+    for i = 1:ng #this step is just to grow the number of edges.
+        if rand() < ϵ
+            pl = find(x->x==0,am[i,:])
+            lg = get_glinks(i_groups[pl[1:end .!= i]],i_groups[i][1][end])
+            if isempty(lg); continue;end
+            link = rand(lg)
+            am[i,link] = 1
+        end #initialize the network, links.
+    end
+    return am
+end
